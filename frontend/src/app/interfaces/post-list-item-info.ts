22d132a59af1860fcd6809076ae9661e153bf47c
agngan <agnieszka.gano@gmail.com>
2021-12-18 19:04:53 +0100

    Added loading image and text from post info

--- frontend/src/app/interfaces/post-list-item-info.ts
+++ frontend/src/app/interfaces/post-list-item-info.ts
@@ -1,9 +1,11 @@
 import {Vote} from "../types/vote";
 
-export interface PostInfo {
+export interface PostListItemInfo {
   title: string;
   subtireddName: string;
   score: number;
   userVote: Vote;
   timestamp: Date;
+  image?: string;
+  text?: string;
 }
