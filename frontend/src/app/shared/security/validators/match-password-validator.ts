d0681b5caef2d7940450f7c328dfff45a2c9a8d8
Bartosz Starosta <bstarosta77@gmail.com>
2021-12-21 19:42:07 +0100

    Minor match password validator fix

--- frontend/src/app/shared/security/validators/match-password-validator.ts
+++ frontend/src/app/shared/security/validators/match-password-validator.ts
@@ -3,5 +3,5 @@ import {AbstractControl, ValidationErrors} from "@angular/forms";
 export function matchPasswordsValidator(group: AbstractControl):  ValidationErrors | null  {
   let pass = group.get('password').value;
   let confirmPass = group.get('confirmPassword').value
-  return pass === confirmPass ? null : { differentPasswords: true }
+  return pass === confirmPass || !confirmPass ? null : { differentPasswords: true }
 }
