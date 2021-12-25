a72fb853b500116f60b074c9efe90538d20108c9
Paweł Cembaluk <241574@student.pwr.edu.pl>
2021-12-25 18:26:27 +0100

    Finished trending today images, added placeholder image when the image is missing.

--- frontend/src/app/home/trending-today/trending-today.component.ts
+++ frontend/src/app/home/trending-today/trending-today.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {TrendingPost} from "../../interfaces/trending-post";
 
 @Component({
   selector: 'trd-trending-today',
@@ -7,11 +8,35 @@ import {Component, OnInit} from '@angular/core';
 })
 export class TrendingTodayComponent implements OnInit {
 
-  trendingPosts = [
-    'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554__340.jpg',
-    'https://cdn.theatlantic.com/thumbor/W544GIT4l3z8SG-FMUoaKpFLaxE=/0x131:2555x1568/1600x900/media/img/mt/2017/06/shutterstock_319985324/original.jpg',
-    'https://i.ytimg.com/vi/1Ne1hqOXKKI/maxresdefault.jpg',
-    'https://www.fearfreehappyhomes.com/wp-content/uploads/2020/08/shutterstock_707431309-e1554172878508.jpg'
+  trendingPosts: TrendingPost[] = [
+    {
+      id: '1',
+      subtirred: 'subtirred1',
+      title: 'Post 1',
+      excerpt: 'Lorem ipsum dolor sit amet, consectetur',
+      imageUrl: 'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554__340.jpg'
+    },
+    {
+      id: '2',
+      subtirred: 'subtirred2',
+      title: 'Post 2',
+      excerpt: 'Lorem ipsum dolor sit amet, consectetur',
+      imageUrl: 'https://cdn.theatlantic.com/thumbor/W544GIT4l3z8SG-FMUoaKpFLaxE=/0x131:2555x1568/1600x900/media/img/mt/2017/06/shutterstock_319985324/original.jpg'
+    },
+    {
+      id: '3',
+      subtirred: 'subtirred3',
+      title: 'Post 3',
+      excerpt: 'Lorem ipsum dolor sit amet, consectetur',
+      imageUrl: 'https://i.ytimg.com/vi/1Ne1hqOXKKI/maxresdefault.jpg'
+    },
+    {
+      id: '4',
+      subtirred: 'subtirred4',
+      title: 'Post 4',
+      excerpt: 'Lorem ipsum dolor sit amet, consectetur',
+      imageUrl: null
+    },
   ]
 
   constructor() {
