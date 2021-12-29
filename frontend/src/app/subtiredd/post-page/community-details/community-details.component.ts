a42136ce26477c8f89d81d400f347ea9749e10f5
Paweł Cembaluk <241574@student.pwr.edu.pl>
2021-12-29 21:10:30 +0100

    Mostly finished community details panel.

--- frontend/src/app/subtiredd/post-page/community-details/community-details.component.ts
+++ frontend/src/app/subtiredd/post-page/community-details/community-details.component.ts
@@ -8,8 +8,11 @@ import {Component, OnInit} from '@angular/core';
 export class CommunityDetailsComponent implements OnInit {
 
   community = {
-    name: 'test',
+    name: 'subtiredd',
     imageUrl: "https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    numberOfMembers: 1000,
+    createDate: new Date("2021-12-18T18:21:00Z")
   }
 
   constructor() {
