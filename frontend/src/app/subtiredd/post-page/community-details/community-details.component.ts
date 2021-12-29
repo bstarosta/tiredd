d7d23c095145723df684cbe340e6cef6835e12c0
Paweł Cembaluk <241574@student.pwr.edu.pl>
2021-12-29 20:49:24 +0100

    Cleaned up unused code in community details.

--- frontend/src/app/subtiredd/post-page/community-details/community-details.component.ts
+++ frontend/src/app/subtiredd/post-page/community-details/community-details.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import {PopularCommunityInfo} from "../../../interfaces/popular-community-info";
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'trd-community-details',
@@ -8,15 +7,13 @@ import {PopularCommunityInfo} from "../../../interfaces/popular-community-info";
 })
 export class CommunityDetailsComponent implements OnInit {
 
-  popularCommunities: PopularCommunityInfo[] = [
-    {name: "first", imageUrl: "https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"},
-    {name: "second", imageUrl: "https://static01.nyt.com/images/2021/09/14/science/07CAT-STRIPES/07CAT-STRIPES-mediumSquareAt3X-v2.jpg"},
-    {name: "third", imageUrl: "https://www.shorturl.at/img/shorturl-icon.png"},
-    {name: "fourth", imageUrl: "https://www.shorturl.at/img/shorturl-icon.png"},
-    {name: "fifth", imageUrl: "https://www.shorturl.at/img/shorturl-icon.png"},
-  ]
+  community = {
+    name: 'test',
+    imageUrl: "https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png",
+  }
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
