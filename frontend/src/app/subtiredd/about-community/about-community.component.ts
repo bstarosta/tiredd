2bf1994b7bada17e64373276bfda0f1e5459740b
agngan <agnieszka.gano@gmail.com>
2021-12-30 11:51:26 +0100

    Fixed problem with taking subtiredd name from route.

--- frontend/src/app/subtiredd/about-community/about-community.component.ts
+++ frontend/src/app/subtiredd/about-community/about-community.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {CreatePostModalService} from "../../services/create-post-modal.service";
+import {AccountModalService} from "../../services/account-modal.service";
 
 @Component({
   selector: 'trd-about-community',
@@ -7,8 +9,19 @@ import {Component} from '@angular/core';
 })
 export class AboutCommunityComponent {
 
+  @Input() isUserLoggedIn: Boolean;
   description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
   numberOfMembers = 1000;
   createDate = new Date("2021-12-18T18:21:00Z");
+
+  constructor(private createPostModalService: CreatePostModalService, private accountModalService: AccountModalService) {
+  }
+
+  onCreatePostClick() {
+    if (this.isUserLoggedIn)
+      this.createPostModalService.openAccountModal();
+    else
+      this.accountModalService.openAccountModal("register");
+  }
 
 }
