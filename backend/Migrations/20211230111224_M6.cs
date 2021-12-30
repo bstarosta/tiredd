20465b6a23fd3d87fbaef52075168893ddfa398e
agngan <agnieszka.gano@gmail.com>
2021-12-30 12:14:33 +0100

    Added description and creation date fields to subtiredd tabe.

---
+++ backend/Migrations/20211230111224_M6.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace backend.Migrations
+{
+    public partial class M6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateDateTime",
+                table: "Subtiredds",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Subtiredds",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreateDateTime",
+                table: "Subtiredds");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Subtiredds");
+        }
+    }
+}
