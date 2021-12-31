cf9d3478bf910bec8ff5b9e76145f3a7d542d8a9
Bartosz Starosta <bstarosta77@gmail.com>
2021-12-31 20:47:06 +0100

    Connected create community modal with backend

--- frontend/src/app/shared/create-community-form/create-community-form.component.ts
+++ frontend/src/app/shared/create-community-form/create-community-form.component.ts
@@ -1,6 +1,7 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 import {FormControl, FormGroup, ValidationErrors, Validators} from "@angular/forms";
 import {CreateCommunityFormOutput} from "../../interfaces/create-community-form-output";
+import {Observable, Subscription} from "rxjs";
 
 const CREATE_COMMUNITY_FORM_ERROR_MESSAGE_KEYS: ValidationErrors = {
   name: {
@@ -21,10 +22,12 @@ const CREATE_COMMUNITY_FORM_ERROR_MESSAGE_KEYS: ValidationErrors = {
   templateUrl: './create-community-form.component.html',
   styleUrls: ['./create-community-form.component.scss']
 })
-export class CreateCommunityFormComponent{
+export class CreateCommunityFormComponent implements OnInit, OnDestroy{
 
   validationErrorsMessageKeys: ValidationErrors = CREATE_COMMUNITY_FORM_ERROR_MESSAGE_KEYS
+  @Input() subtireddNameConflict$: Observable<void>
   @Output() formSubmitted: EventEmitter<CreateCommunityFormOutput> = new EventEmitter<CreateCommunityFormOutput>();
+  subtireddNameConflictSubscription: Subscription;
 
   form: FormGroup = new FormGroup({
     name: new FormControl("", [Validators.required, Validators.maxLength(20)]),
@@ -42,11 +45,21 @@ export class CreateCommunityFormComponent{
   }
 
   get imageUrl() {
-    return this.form.get("imageUrl")
+    return this.form.get("imageUrl");
   }
 
   onSubmit(): void {
     this.formSubmitted.emit(this.form.value);
+  }
+
+  ngOnInit(): void {
+    this.subtireddNameConflictSubscription = this.subtireddNameConflict$.subscribe( _ => {
+      this.name.setErrors({conflict: true});
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.subtireddNameConflictSubscription.unsubscribe();
   }
 
 }
--- frontend/src/app/shared/create-community-modal/create-community-modal.component.ts
+++ frontend/src/app/shared/create-community-modal/create-community-modal.component.ts
@@ -2,6 +2,9 @@ import {Component, Inject} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {CreateCommunityFormOutput} from "../../interfaces/create-community-form-output";
 import {SnackbarService} from "../../services/snackbar.service";
+import {SubtireddService} from "../../services/subtiredd.service";
+import {Observable} from "rxjs";
+import {take} from "rxjs/operators";
 
 @Component({
   selector: 'trd-create-community-modal',
@@ -11,15 +14,24 @@ import {SnackbarService} from "../../services/snackbar.service";
 export class CreateCommunityModalComponent {
 
   constructor(@Inject(MAT_DIALOG_DATA) data: string, private matDialogRef: MatDialogRef<CreateCommunityModalComponent>,
-              private snackbarService: SnackbarService) {
+              private snackbarService: SnackbarService, private subtireddService: SubtireddService) {
+
+    this.subtireddService.subtireddCreated$.pipe(take(1)).subscribe(_ => this.onCreatedSubtiredd());
+    this.subtireddNameConflict$ = subtireddService.subtireddNameConflict$;
 
   }
 
+  subtireddNameConflict$: Observable<void>
+
   onFormSubmit(communityData: CreateCommunityFormOutput) {
-    this.onCloseClick();
+    this.subtireddService.createSubtiredd(communityData)
   }
 
   onCloseClick() {
+    this.matDialogRef.close();
+  }
+
+  onCreatedSubtiredd() {
     this.snackbarService.openSuccessSnackbar("createCommunitySuccess")
     this.matDialogRef.close();
   }
