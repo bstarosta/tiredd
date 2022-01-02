6fd277d138f1ffd952c859092e6d0949de1f87f7
agngan <agnieszka.gano@gmail.com>
2022-01-02 17:24:52 +0100

    Changed comment controller url.

--- backend/Controllers/CommentController.cs
+++ backend/Controllers/CommentController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers
 {
-    [Route("api/comments")]
+    [Route("api/comment")]
     [ApiController]
     public class CommentController : TireddController
     {
