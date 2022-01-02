183482b75d03e3f7796c021a2aad88693d4b3bd5
Paweł Cembaluk <241574@student.pwr.edu.pl>
2022-01-02 11:56:01 +0100

    Minor subtiredd page data model change.

--- frontend/src/app/subtiredd/subtiredd-page/subtiredd-page.component.ts
+++ frontend/src/app/subtiredd/subtiredd-page/subtiredd-page.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {UserService} from "../../services/user.service";
 import {Observable, Subscription} from "rxjs";
+import {Subtiredd} from "../../interfaces/subtiredd";
 
 @Component({
   selector: 'trd-subtiredd-page',
@@ -13,12 +14,17 @@ export class SubtireddPageComponent implements OnDestroy {
   isUserLoggedIn$: Observable<Boolean>;
   routeSubscription: Subscription;
 
-  // TODO: Take subtiredd data from API
-  subtireddId: number = 1
-  subtireddName: string
+  subtiredd: Subtiredd = {
+    id: 1,
+    name: null,
+    description: "Description",
+    imageUrl: "https://i.ytimg.com/vi/1Ne1hqOXKKI/maxresdefault.jpg",
+    createdAt: new Date("2021-12-18T18:21:00Z"),
+    adminId: '1'
+  }
 
   constructor(private route: ActivatedRoute, private userService: UserService) {
-    this.routeSubscription = route.paramMap.subscribe(paramMap => this.subtireddName = paramMap.get("subtireddName"));
+    this.routeSubscription = route.paramMap.subscribe(paramMap => this.subtiredd.name = paramMap.get("subtireddName"));
     this.isUserLoggedIn$ = userService.isUserLoggedIn$;
   }
 
