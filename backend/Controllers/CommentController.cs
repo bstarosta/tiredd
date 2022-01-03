7640cabd724c1de772a5d1296f816cf892f87ff8
agngan <agnieszka.gano@gmail.com>
2022-01-03 20:23:01 +0100

    Implemented endpoint for getting comments.

--- backend/Controllers/CommentController.cs
+++ backend/Controllers/CommentController.cs
@@ -1,12 +1,16 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using backend.Controllers.Models;
+using backend.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend.Controllers
 {
-    [Route("api/comment")]
+    [Route("api/comments")]
     [ApiController]
     public class CommentController : TireddController
     {
@@ -25,5 +29,37 @@ namespace backend.Controllers
                 return new ObjectResult(createdComment.Entity) {StatusCode = StatusCodes.Status201Created};
             }
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(int postId)
+        {
+            await using (tireddDbContext)
+            {
+                var comments = await tireddDbContext.Comments
+                    .Include(c => c.ChildComments)
+                    .Include(c => c.Author)
+                    .AsQueryable()
+                    .Where(c => c.PostId == postId)
+                    .ToListAsync();
+                var commentViewModels = ListToCommentJson(comments)
+                    .Where(c => c.ParentCommentId == null);
+                return new ObjectResult(commentViewModels) {StatusCode = StatusCodes.Status200OK};
+            }
+        }
+
+        private static List<CommentViewModel> ListToCommentJson(List<Comment> comments)
+        {
+            return comments.Select(comment =>
+                    new CommentViewModel(
+                        comment.Id,
+                        comment.Text,
+                        comment.Author.UserName,
+                        comment.CreatedAt,
+                        comment.ParentCommentId,
+                        comment.ChildComments.Count == 0
+                            ? new List<CommentViewModel>()
+                            : ListToCommentJson(comment.ChildComments)))
+                .ToList();
+        } 
     }
 }
